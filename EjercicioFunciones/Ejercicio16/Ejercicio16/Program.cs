74f7685817b4a9f867a7014f8e4e1ffddc7dcff9
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:56:31 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio16/Ejercicio16/Program.cs
@@ -0,0 +1,8 @@
+﻿// Crear una función que reciba una cantidad de minutos y devuelva el equivalente en horas.
+Console.Write("Ingrese la cantidad de minutos: ");
+double minutos = double.Parse(Console.ReadLine());
+Console.WriteLine($"El equivalente en horas es: {ConvertirAHoras(minutos)}");
+double ConvertirAHoras(double m)
+{
+    return m / 60;
+}
