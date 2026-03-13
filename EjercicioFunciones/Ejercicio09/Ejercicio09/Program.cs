ebbd7d093da28aa9eeaa82704bc367cf06e58421
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:53:58 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio09/Ejercicio09/Program.cs
@@ -0,0 +1,10 @@
+﻿//Crear una función que reciba el salario por hora y las horas trabajadas y devuelva el salario total.
+Console.WriteLine("ingrese el salario por hora: ");
+int salario = int .Parse(Console.ReadLine());
+Console.WriteLine("ingrese las horas trabajadas ");
+int horas = int.Parse(Console.ReadLine());
+Console.WriteLine($"el salario total es: {SalarioTotal(salario, horas)}");
+int SalarioTotal(int s, int h)
+{
+    return s * h;
+}
