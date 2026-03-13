d1e241a592258b73245c21a4acbdd561d4265106
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:55:48 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio14/Ejercicio14/Program.cs
@@ -0,0 +1,10 @@
+﻿//Crear una función que reciba la distancia recorrida y el tiempo empleado y devuelva la velocidad promedio.
+Console.Write("Ingrese la distancia recorrida ");
+double distancia = double.Parse(Console.ReadLine());
+Console.WriteLine("ingrese el tiempo empleado");
+double tiempo = double.Parse(Console.ReadLine());
+Console.WriteLine($"la velocidad promedio es: {VelocidadPromedio (distancia,tiempo)}");
+double VelocidadPromedio(double d, double t)
+{
+    return d / t;
+}
