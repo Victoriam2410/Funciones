4de02c64194ba328eebb28f2871f2ce946ed170e
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 23:00:31 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio28/Ejercicio28/Program.cs
@@ -0,0 +1,17 @@
+﻿//Crear un procedimiento que solicite tres calificaciones y muestre el promedio del estudiante.
+CalcularPromedio();
+void CalcularPromedio()
+{
+    Console.WriteLine("Ingrese la primera calificación: ");
+    double numero1 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese la segunda calificación: ");
+    double numero2 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese la tercera calificación: ");
+    double numero3 = double.Parse(Console.ReadLine());
+
+    double promedio = (numero1 + numero2 + numero3) / 3;
+
+    Console.WriteLine($"El promedio final es: {promedio}");
+}
