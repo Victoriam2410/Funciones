62803d99ac955127280335000e3d427e87459533
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:52:30 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio05/Ejercicio05/Program.cs
@@ -0,0 +1,10 @@
+﻿//Crear una función que reciba la base y la altura de un rectángulo y devuelva su área
+Console.WriteLine("ingrese la base:");
+double basee = double. Parse(Console.ReadLine());
+Console.WriteLine("ingrese la altura:");
+double altura = double.Parse(Console.ReadLine());
+Console.WriteLine($"el area del circulo es : {area (basee, altura)}");
+double area (double a, double b)
+{
+    return a * b;
+}
