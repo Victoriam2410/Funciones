06528392611b362dffc5d2620663a363f1207747
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 23:00:47 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio29/Ejercicio29/Program.cs
@@ -0,0 +1,10 @@
+﻿// Crear un procedimiento que solicite el número de estudiantes de un curso y muestre un mensaje indicando cuántos estudiantes hay en la clase.
+ConteoEstudiantes();
+
+void ConteoEstudiantes()
+{
+    Console.WriteLine("Cuántos estudiantes hay en el curso? ");
+    int cantidad = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Hay un total de {cantidad} estudiantes en la clase.");
+}
