5950d51683b51ba1defcb7b868026e3aa2ddf49e
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:57:38 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio19/Ejercicio19/Program.cs
@@ -0,0 +1,14 @@
+﻿// Crear una función que reciba el nombre de un estudiante y tres calificaciones y devuelva el promedio final.
+Console.Write("Ingrese el nombre del estudiante: ");
+string nombre = Console.ReadLine();
+Console.Write("Ingrese la primera nota: ");
+double numero1 = double.Parse(Console.ReadLine());
+Console.Write("Ingrese la segunda nota: ");
+double numero2 = double.Parse(Console.ReadLine());
+Console.Write("Ingrese la tercera nota: ");
+double numero3 = double.Parse(Console.ReadLine());
+Console.WriteLine($"El promedio de {nombre} es: {Promedio(numero1, numero2, numero3)}");
+double Promedio(double a, double b, double c)
+{
+    return (a + b + c) / 3;
+}
