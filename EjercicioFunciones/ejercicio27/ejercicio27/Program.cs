3ded72f582cf269fe7dc23ae29414d764aecd774
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 23:00:13 -0600

    Add files via upload

---
+++ EjercicioFunciones/ejercicio27/ejercicio27/Program.cs
@@ -0,0 +1,12 @@
+﻿// Crear un procedimiento que solicite el precio de un producto y muestre el precio con un descuento del 10%.
+CalcularPrecio();
+void CalcularPrecio()
+{
+    Console.Write("Ingrese el precio del producto: ");
+    double precio = double.Parse(Console.ReadLine());
+
+    double descuento = precio * 0.10;
+    double total = precio - descuento;
+
+    Console.WriteLine($"El precio con el 10% de descuento es: {total}");
+}
