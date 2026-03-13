4f6a9ced95e1d25dc986245541c7ff00b4ac7e47
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:58:37 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio22/Ejercicio22/Program.cs
@@ -0,0 +1,8 @@
+﻿// Crear un procedimiento que solicite el nombre de una persona y muestre un saludo personalizado.
+SaludoPersonalizado();
+void SaludoPersonalizado()
+{
+    Console.Write("Ingrese su nombre: ");
+    string nombre = Console.ReadLine();
+    Console.WriteLine($"Hola {nombre}, un gusto saludarte.");
+}
