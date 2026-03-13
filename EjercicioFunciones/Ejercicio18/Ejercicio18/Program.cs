d4ba6224a72ab8cd8f36a7f923f744c63431afe3
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:57:21 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio18/Ejercicio18/Program.cs
@@ -0,0 +1,15 @@
+﻿// Crear una función que reciba la cantidad de productos comprados y el precio de cada uno y devuelva el total de la factura.
+Console.Write("Cuántos productos compró? ");
+int cantidad = int.Parse(Console.ReadLine());
+Console.WriteLine($"el total de la factura es: {TotalFactura(cantidad)}");
+double TotalFactura(int c)
+{
+    double total = 0;
+    for (int i = 0; i < c; i++)
+    {
+        Console.Write("Ingrese el precio del producto: ");
+        double precio = double.Parse(Console.ReadLine());
+        total += precio;
+    }
+    return total;
+}
