88d28b3519c58964fb07ae5062699d65568f7561
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:58:19 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio21/Ejercicio21/Program.cs
@@ -0,0 +1,8 @@
+﻿// Crear un procedimiento que muestre en pantalla un mensaje de bienvenida al usuario.
+Console.WriteLine("Ingrese su nombre: ");
+string usuario = Console.ReadLine();
+Console.WriteLine(Bienvenida(usuario));
+string Bienvenida(string nombre)
+{
+    return $"Bienvenido al sistema: {nombre}";
+}
