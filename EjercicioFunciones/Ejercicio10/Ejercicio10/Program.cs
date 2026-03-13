d66163a9e89afe0acd2de4e5077bfba6bd2bbbd2
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:54:19 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio10/Ejercicio10/Program.cs
@@ -0,0 +1,8 @@
+﻿//Crear una función que reciba un número y devuelva el cubo del número.
+Console.WriteLine("ingrese el numero: ");
+int numero = int.Parse(Console.ReadLine());
+Console.WriteLine($"el cubo del numero: {Cubo(numero)}");
+int Cubo (int n)
+{
+    return n * n * n;
+}
