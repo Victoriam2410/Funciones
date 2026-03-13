bad734f6e0d2d3af5c1d9b79cb6e90d03010f18c
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:52:09 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio04/Ejercicio04/Program.cs
@@ -0,0 +1,9 @@
+﻿// Crear una función que reciba el radio de un círculo y devuelva el área del círculo.
+Console.WriteLine("ingrese el radio del circulo: ");
+double radio= double.Parse(Console.ReadLine());
+Console.WriteLine($"el area es: {area (radio)}");
+double area (double r)
+{
+    return 3.14 * (r* r);
+}
+
