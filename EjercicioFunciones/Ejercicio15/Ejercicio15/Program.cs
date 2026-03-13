7c5dc937a4b53c2a4fbb87fe30dcf9b3a2918db7
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:56:12 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio15/Ejercicio15/Program.cs
@@ -0,0 +1,10 @@
+﻿//Crear una función que reciba la base y la altura de un triángulo y devuelva su área.
+Console.Write("Ingrese la base: ");
+double basee = double.Parse(Console.ReadLine());
+Console.Write("Ingrese la altura: ");
+double altura= double.Parse(Console.ReadLine());
+Console.WriteLine($"El área del triángulo es: {Area(basee, altura)}");
+double Area (double b,double a)
+{
+    return (b * a) / 2;
+}
