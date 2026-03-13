baa6b67326a021456045cdee54f3fe4f1f663075
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:54:40 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio11/Ejercicio11/Program.cs
@@ -0,0 +1,8 @@
+﻿// Crear una función que reciba el peso en kilogramos y devuelva el peso en libras.
+Console.WriteLine("ingrese el peso (Kg): ");
+double Kilogramos = double.Parse(Console.ReadLine());
+Console.WriteLine($"el peso en libras es: {Peso(Kilogramos)} lb");
+double Peso(double kg )
+{
+    return kg*2.205;
+}
