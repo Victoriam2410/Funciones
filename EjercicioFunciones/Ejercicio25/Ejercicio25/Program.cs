d9b883760db827223bc422e90db111c5bb1db8cc
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:59:38 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio25/Ejercicio25/Program.cs
@@ -0,0 +1,14 @@
+﻿// Crear un procedimiento que solicite la base y la altura de un rectángulo y muestre su área.
+CalcularArea();
+void CalcularArea()
+{
+    Console.Write("Ingrese la base: ");
+    double b = double.Parse(Console.ReadLine());
+
+    Console.Write("Ingrese la altura: ");
+    double h = double.Parse(Console.ReadLine());
+
+    double area = b * h;
+
+    Console.WriteLine($"el área del rectangulo es: {area}");
+}
