a5168e1b9c275e90861e4fb36c60de3414554b4e
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:53:14 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio07/Ejercicio07/Program.cs
@@ -0,0 +1,10 @@
+﻿// Crear una función que reciba el precio de un producto y el porcentaje de descuento y devuelva el precio final.
+Console.WriteLine("ingrese precio: ");
+double precio = double. Parse(Console.ReadLine());
+Console.WriteLine("ingrese el porcentaje de descuento:");
+double porsentaje = double.Parse(Console.ReadLine());
+Console.WriteLine($"total {total(precio,porsentaje)}");
+double total (double p, double t)
+    {
+    return (p * t) / 100;
+    }
