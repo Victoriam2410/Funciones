e97d65ed0cc25f3f12c41e95f2b65c9c40f5b61f
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:55:26 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio13/Ejercicio13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Crear una función que reciba el precio de varios productos y devuelva el total de la compra.
+Console.Write("Cuántos productos desea ingresar? ");
+int cantidad = int.Parse(Console.ReadLine());
+
+double total = TotalCompra(cantidad);
+
+Console.WriteLine($"El total de la compra es: {total}");
+
+double TotalCompra(int c)
+{
+    double t = 0;
+    for (int i = 0; i < c; i++)
+    {
+        Console.Write("Ingrese el precio: ");
+        double precio = double.Parse(Console.ReadLine());
+        t += precio;
+    }
+    return t;
+}
