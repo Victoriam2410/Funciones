a6f7d5c25ba13e9f96322250dbfe74d2e50b0de0
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:59:11 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio24/Ejercicio24/Program.cs
@@ -0,0 +1,11 @@
+﻿// Crear un procedimiento que solicite el radio de un círculo y muestre el área del círculo.
+CalcularArea();
+void CalcularArea()
+{
+    Console.Write("Ingrese el radio del círculo: ");
+    double radio = double.Parse(Console.ReadLine());
+
+    double area = Math.PI * Math.Pow(radio, 2);
+
+    Console.WriteLine($"el área del círculo es: {area}");
+}
