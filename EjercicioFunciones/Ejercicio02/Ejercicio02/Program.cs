9526ee9ddafcee9b2900ebacd4a83b599465b2c3
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:51:21 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio02/Ejercicio02/Program.cs
@@ -0,0 +1,8 @@
+﻿// Crear una función que reciba un número y devuelva su cuadrado.
+Console.WriteLine("ingrese numero");
+int numero = int .Parse(Console.ReadLine());
+Console.WriteLine($"su cuadrado es: {cuadrado(numero)}");
+int cuadrado (int a)
+{
+    return a * a;
+}
