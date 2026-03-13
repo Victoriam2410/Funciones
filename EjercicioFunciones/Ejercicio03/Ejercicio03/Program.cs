80e6df53d7d578eb348a99aa435c114a131396fa
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:51:45 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio03/Ejercicio03/Program.cs
@@ -0,0 +1,8 @@
+﻿// Crear una función que reciba un número y devuelva su doble.
+Console.WriteLine("ingrese numero");
+int numero= int .Parse(Console.ReadLine());
+Console.WriteLine($"el doble del numero es: {doble (numero)}");
+int doble(int n)
+{
+    return n * 2;
+}
