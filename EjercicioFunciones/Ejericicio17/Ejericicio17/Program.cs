63ff4a6e1c4595049799af30dbbd9f0e967492d2
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:56:55 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejericicio17/Ejericicio17/Program.cs
@@ -0,0 +1,17 @@
+﻿//Crear una función que reciba la cantidad de estudiantes y la nota de cada uno y devuelva el promedio del grupo.
+Console.Write("Ingrese la cantidad de estudiantes: ");
+int cantidad = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"El promedio del grupo es: {Promedio(cantidad)}");
+
+double Promedio(int c)
+{
+    double suma = 0;
+    for (int i = 0; i < c; i++)
+    {
+        Console.Write("Ingrese la nota: ");
+        double nota = double.Parse(Console.ReadLine());
+        suma += nota;
+    }
+    return suma / c;
+}
