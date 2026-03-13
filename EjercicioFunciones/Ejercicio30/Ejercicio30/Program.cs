62116863d5a7771d8cbb1dbfaf1ca705c1466353
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 23:01:04 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio30/Ejercicio30/Program.cs
@@ -0,0 +1,27 @@
+﻿// Crear un procedimiento que solicite el nombre de un estudiante, su curso y tres calificaciones, y muestre un reporte simple con toda la información ingresada.
+ReporteEstudiante();
+
+void ReporteEstudiante()
+{
+    Console.WriteLine("Ingrese el nombre del estudiante: ");
+    string nombre = Console.ReadLine();
+
+    Console.WriteLine("Ingrese el curso: ");
+    string curso = Console.ReadLine();
+
+    Console.WriteLine("Ingrese la primera calificación: ");
+    double n1 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese la segunda calificación: ");
+    double n2 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese la tercera calificación: ");
+    double n3 = double.Parse(Console.ReadLine());
+
+    double promedio = (n1 + n2 + n3) / 3;
+    Console.WriteLine("El reporte de estudiante es: ");
+    Console.WriteLine($"Nombre: {nombre}");
+    Console.WriteLine($"Curso: {curso}");
+    Console.WriteLine($"Calificaciones: {n1}, {n2}, {n3}");
+    Console.WriteLine($"Promedio Final: {promedio}");
+}
