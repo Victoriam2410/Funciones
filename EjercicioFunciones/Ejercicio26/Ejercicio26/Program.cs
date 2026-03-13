97537c8712bfe1d02bc59caaba765d6023135276
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:59:53 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio26/Ejercicio26/Program.cs
@@ -0,0 +1,14 @@
+﻿// Crear un procedimiento que solicite la edad de una persona y muestre el año aproximado de nacimiento.
+CalcularNacimiento();
+void CalcularNacimiento()
+{
+    Console.Write("Ingrese el año actual: ");
+    int Actual = int.Parse(Console.ReadLine());
+
+    Console.Write("Ingrese su edad: ");
+    int edad = int.Parse(Console.ReadLine());
+
+    int nacimiento = Actual - edad;
+
+    Console.WriteLine($"Su año aproximado de nacimiento es: {nacimiento}");
+}
