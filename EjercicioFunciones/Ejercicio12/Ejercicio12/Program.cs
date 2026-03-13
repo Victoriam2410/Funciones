b918192809634ba4facedd28f80d9144afa6e738
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:55:02 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio12/Ejercicio12/Program.cs
@@ -0,0 +1,8 @@
+﻿//Crear una función que reciba la edad de una persona y devuelva un mensaje mostrando la edad ingresada.
+Console.WriteLine("ingrese la edad de la persona: ");
+int edad = int.Parse(Console.ReadLine());
+Console.WriteLine($"la edad de la persona es: {EdadIngresada(edad)}");
+int EdadIngresada (int e)
+{
+    return e;
+}
