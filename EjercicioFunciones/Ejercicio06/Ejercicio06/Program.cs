3f54daab9fb79a25ac56b507c821daadb594971b
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:52:52 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio06/Ejercicio06/Program.cs
@@ -0,0 +1,9 @@
+﻿//Crear una función que reciba un número en grados Celsius y devuelva el valor convertido a Fahrenheit.
+//(0 °C × 9/5) + 32 = 32 °F
+Console.WriteLine("ingrese grados Celsius :");
+int celsius = int. Parse(Console.ReadLine());
+Console.WriteLine($"{conversion(celsius)}°F");
+int conversion (int c)
+{
+    return (c * 9 / 5) + 32;
+}
