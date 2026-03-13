8b3badbef2ae0ec7fbc134ec67bbd91c77ce4989
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:50:51 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio01/Ejercicio01/Program.cs
@@ -0,0 +1,11 @@
+﻿//Crear una función que reciba dos números y devuelva la suma de ambos.
+Console.WriteLine("ingrese dos numero \n");
+Console.WriteLine("ingrese numero uno");
+int numero1= int.Parse(Console.ReadLine());
+Console.WriteLine("ingrese numero dos");
+int numero2 = int.Parse(Console.ReadLine());
+Console.WriteLine($"la usma es: {Sumar(numero1, numero2)}");
+int Sumar (int a, int b)
+{
+    return a + b;
+}
