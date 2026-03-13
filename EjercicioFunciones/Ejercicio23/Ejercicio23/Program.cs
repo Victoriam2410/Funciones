67839feed522da2851c9954ecc2cfadc65792ca1
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:58:53 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio23/Ejercicio23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Crear un procedimiento que solicite dos números al usuario y muestre la suma de ambos.
+SumarNumeros();
+void SumarNumeros()
+{
+    Console.Write("Ingrese el primer número: ");
+    double n1 = double.Parse(Console.ReadLine());
+
+    Console.Write("Ingrese el segundo número: ");
+    double n2 = double.Parse(Console.ReadLine());
+
+    double suma = n1 + n2;
+    Console.WriteLine($"la suma de ambos números es: {suma}");
+}
