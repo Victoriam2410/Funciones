372c4d85a9e16de2a101704d7708afe30c00484d
Victoriam2410 <estherrodas61@gmail.com>
2026-03-12 22:57:54 -0600

    Add files via upload

---
+++ EjercicioFunciones/Ejercicio20/Ejercicio20/Program.cs
@@ -0,0 +1,13 @@
+﻿// Crear una función que reciba la cantidad de días trabajados y el pago diario y devuelva el salario total del empleado.
+Console.Write("ingrese la cantidad de días trabajados: ");
+int dias = int.Parse(Console.ReadLine());
+
+Console.Write("ingrese el pago diario: ");
+double pago = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"el salario total del empleado es: {SalarioTotal(dias, pago)}");
+
+double SalarioTotal(int d, double p)
+{
+    return d * p;
+}
